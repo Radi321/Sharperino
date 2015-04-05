c9c0d603f299ba11b0f1fdd0346db18fa7f58b37
Radi321 <radimpc@yahoo.com>
2015-04-05 13:08:43 +0200

    xaxax
    scripts

---
+++ RadiSurrender/RadiSurrender/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace RadiSurrender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        static void Game_OnUpdate(EventArgs args)
+        {
+            Game.Say("/ff");
+        }
+    }
+}
