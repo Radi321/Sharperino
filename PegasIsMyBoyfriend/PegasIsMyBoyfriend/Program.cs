7a71f3e3edaca26c1ff411c92c774f60daa7cf30
Radi321 <radimpc@yahoo.com>
2015-04-06 16:29:53 +0200

    new script
    xoxo

---
+++ PegasIsMyBoyfriend/PegasIsMyBoyfriend/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+
+namespace PegasIsMyBoyfriend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        static void Game_OnUpdate(EventArgs args)
+        {
+            Game.PrintChat("Pegas is your boyfriend!");
+            Game.Say("Pegas love me!");
+        }
+    }
+}
